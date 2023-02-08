1bb94549ef1c3231e31ae628892a2f93c5efefcf
Kjetil Bugge Kristoffersen <kjetil.kristoffersen@autodesk.com>
2023-02-08 11:11:17 +0100

    chore: cleanup unused files

--- src/controls.ts
+++
@@ -1,9 +0,0 @@
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
-import Grid from "./Grid";
-import { Camera, Renderer } from "three";
-
-export function setupControls(camera: Camera, renderer: Renderer) {
-  const controls = new OrbitControls(camera, renderer.domElement);
-  controls.target.set(Grid.center.x, Grid.center.y, 0);
-  controls.update();
-}
